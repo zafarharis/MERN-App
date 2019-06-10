15e9d68cb1d7d3ff8e71d438030ab543b2d4d3df
Muhammad Haris Zafar <haris_zafar@outlook.com>
2019-06-10 13:43:34 +0500

    Add files via upload

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import {
   Collapse,
   Navbar,
@@ -6,40 +6,79 @@ import {
   NavbarBrand,
   Nav,
   NavItem,
-  NavLink,
   Container
 } from 'reactstrap';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import RegisterModal from './auth/RegisterModal';
+import LoginModal from './auth/LoginModal';
+import Logout from './auth/Logout';
 
 class AppNavbar extends Component {
-    state = {
-        isOpen: false
-    };
+  state = {
+    isOpen: false
+  };
 
-    toggle = () => {
-        this.setState({
-          isOpen: !this.state.isOpen
-        });
-    };
+  static propTypes = {
+    auth: PropTypes.object.isRequired
+  };
 
-    render(){
-        return (
-            <div>
-              <Navbar color='dark' dark expand='sm' className='mb-5'>
-                <Container>
-                  <NavbarBrand href='/'>ShoppingList</NavbarBrand>
-                  <NavbarToggler onClick={this.toggle} />
-                  <Collapse isOpen={this.state.isOpen} navbar>
-                    <Nav className='ml-auto' navbar>
-                    <NavItem>
-                        <NavLink href="https://facebook.com">facebook</NavLink>
-                    </NavItem>
-                    </Nav>
-                  </Collapse>
-                </Container>
-              </Navbar>
-            </div>
-        );
-    }
+  toggle = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  };
+
+  render() {
+    const { isAuthenticated, user } = this.props.auth;
+
+    const authLinks = (
+      <Fragment>
+        <NavItem>
+          <span className='navbar-text mr-3'>
+            <strong>{user ? `Welcome ${user.name}` : ''}</strong>
+          </span>
+        </NavItem>
+        <NavItem>
+          <Logout />
+        </NavItem>
+      </Fragment>
+    );
+
+    const guestLinks = (
+      <Fragment>
+        <NavItem>
+          <RegisterModal />
+        </NavItem>
+        <NavItem>
+          <LoginModal />
+        </NavItem>
+      </Fragment>
+    );
+
+    return (
+      <div>
+        <Navbar color='dark' dark expand='sm' className='mb-5'>
+          <Container>
+            <NavbarBrand href='/'>ShoppingList</NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className='ml-auto' navbar>
+                {isAuthenticated ? authLinks : guestLinks}
+              </Nav>
+            </Collapse>
+          </Container>
+        </Navbar>
+      </div>
+    );
+  }
 }
 
-export default AppNavbar;
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(
+  mapStateToProps,
+  null
+)(AppNavbar);
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const config = require('config');
 
 const app = express();
 
@@ -8,7 +9,7 @@ const app = express();
 app.use(express.json());
 
 // DB Config
-const db = require('./config/keys').mongoURI;
+const db = config.get('mongoURI');
 
 // Connect to Mongo
 mongoose
@@ -21,6 +22,8 @@ mongoose
 
 // Use Routes
 app.use('/api/items', require('./routes/api/items'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
 
 const port = process.env.PORT || 5000;
 
